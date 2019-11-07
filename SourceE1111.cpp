4a5222acf3b5661e74a36e085a2b4475bb79396c
Volodymir Konovalov <vvkon@meta.ua>
2019-11-07 08:20:12 +0200

    Update SourceE1111.cpp

--- SourceE1111.cpp
+++ SourceE1111.cpp
@@ -1,7 +1,16 @@
 #include "../std_lib_facilities.h"
 
+vector<string> split(const string& s, const string& w);
+
 int main()
 {
 
     return 0;
+}
+
+vector<string>split(const string& s, const string& w)
+{
+
+
+    return   ;
 }
