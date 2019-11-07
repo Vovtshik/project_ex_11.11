4ee3e5cbcbb9cd112b618e3bda162322d4382cdf
Volodymir Konovalov <vvkon@meta.ua>
2019-11-07 09:31:11 +0200

    Update SourceE1111.cpp

--- SourceE1111.cpp
+++ SourceE1111.cpp
@@ -10,7 +10,27 @@ int main()
 
 vector<string>split(const string& s, const string& w)
 {
-
-
-    return   ;
+    vector<string> vs;
+    stringstream ss(s);
+    string str;
+    for(string st; ss >> st;)
+    {
+        for(char ch: st)
+        {
+            for(char ch1: w)
+            {
+                if(ch == ch1 )
+                    str += ' ';
+                else
+                    str += ch;
+            }
+        }
+        str += ' ';
+    }
+    stringstream ss2(str);
+    for(string st2; ss2 >> st2;)
+    {
+        vs.push_back(st2);
+    }
+    return vs;
 }
